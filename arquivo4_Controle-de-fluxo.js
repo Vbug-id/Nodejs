2997944f5328bda70b16708a83d608ca70c9d15d
Vbug-id <viniidfn@gmail.com>
2025-01-04 23:43:46 -0300

    atualizando!

--- arquivo4_Controle-de-fluxo.js
+++ arquivo4_Controle-de-fluxo.js
@@ -1,6 +1,31 @@
 const num1 = 100
-//const num2 = 20
+const num2 = 20
+const num3 = 7
+const num4 = '7' // este numero é uma string
 
+// Exemplo de condicionais simples
+// < Menos, > Maior, <= Menor ou igual, >= Maior ou Igual, === Igual,
 if (num1 > 1) {
     console.log('Este número é maior que 1')
+} 
+if ( num2 < 10) {
+    console.log('Este número é menor que 10')
+} else {
+    console.log('Este número é maior que 10')
+}
+
+//Exemplo de condicionais comparativas 
+
+if (num1 < num2) {
+    console.log(`${num1} é menor que o ${num2}`)
+} else {
+    console.log(`${num2} é menor que o ${num1}`)
+}
+
+// Exemplo de igualdade; não utilizar == para igualdade ha problemas em conversões implicitas de string e numeros 
+
+if ( num1 === num2){
+    console.log('os numeros são iguais em valor e tipo')
+} else {
+    console.log('os numeros são iguais porém não são do mesmo tipo') // os numeros são iguais porém não são do mesmo tipo
 }
