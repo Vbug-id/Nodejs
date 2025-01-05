ddac5d635d70880318503603b8a32a396b1269c4
Vbug-id <viniidfn@gmail.com>
2025-01-05 14:30:28 -0300

    looping for

---
+++ For-of.js
@@ -0,0 +1,27 @@
+const listNumber = [-3, -2, -1, 0, 1, 2, 3]
+const usuarios = ['Maria','José']
+
+// imperativo
+//for (let index = 0; index < listNumber.length; index ++) {
+//   console.log(listNumber[index]);
+//}
+
+//comparativo while
+//console.log('Print via while \n');
+//let indiceNumber = 0
+//while (indiceNumber < listNumber.length){
+//    console.log(listNumber[indiceNumber])
+//    indiceNumber =++ indiceNumber
+//}
+
+
+
+//declarativo
+for (const numero of listNumber){
+    console.log(numero);
+}
+
+for (const usuario of usuarios){
+    console.log(usuario);
+}
+
---
+++ arquivo8_For.js
@@ -0,0 +1,15 @@
+// extrura e sintax do for, simples
+
+for (let contador = 0; contador < 101; contador ++) {
+    console.log(contador);
+}
+console.log('\n');
+
+//Caso 2
+
+const listNumber = [-3, -2, -1, 0, 1, 2, 3]
+
+for (let index = 0; index < listNumber.length; index ++) {
+    console.log(listNumber[index]);
+
+}
