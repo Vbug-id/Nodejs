2a4702593055658a132382972adc4e2f811dd89f
Vbug-id <viniidfn@gmail.com>
2025-01-05 00:10:36 -0300

    boolean

---
+++ arquivo5_Operadores-Booleanos.js
@@ -0,0 +1,28 @@
+
+
+if (true){
+    console.log('Sempre Aparecerei !')
+}
+
+if (false){
+    console.log('Nunca Aparecerei !')
+}
+
+//console.log(100>20)
+//console.log(100>=20)
+//console.log(100<20)
+//console.log(100<=20)
+
+// Operador logico, || = OU, OR, ; se utilizar apenas 1 | o retorno será binario; Se haver uma condição verdadeira o retorno sempre será verdadeiro
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || false)
+console.log(false || true)
+
+// Operador logico, && = E, AND; se utilizar apenas 1 & o retorno será binario
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && false)
+console.log(false && true)
