e33c29f02cbfe78404a575b2f9a63a900a4915ff
Vbug-id <viniidfn@gmail.com>
2025-01-05 21:10:08 -0300

    organizando_add_nodejs

---
+++ nodejs/io-prompt.js
@@ -0,0 +1,26 @@
+const { resolve } = require('path')
+const rl = require('readline') 
+
+const prompt = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const promptPromise = {
+    question: (pergunta) => new Promise((resolve, reject) => {
+        try{
+            prompt.question((pergunta), (resposta) => resolve(resposta))
+        } catch (error){
+            reject(error)
+        }
+    }),
+    close: prompt.close
+}
+
+async function askUser() {
+    const numero = await promptPromise.question('Qual seu número favorito ?: ')
+    console.log(`O dobro do seu número favorito é: ${parseInt(numero) * 2}`);
+}
+
+askUser()
+
